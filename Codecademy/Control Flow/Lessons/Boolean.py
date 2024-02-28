64b49b62c6193e4258e6370821d6dd7d9029f156
Alfie <alfietones0673@outlook.com>
2024-02-28 14:46:09 +0000

    Overview of Booleans

---
+++ Codecademy/Control Flow/Lessons/Boolean.py
@@ -0,0 +1,16 @@
+# Determine if the following statements are boolean expressions or not. If they are, set the matching variable to the right to "Yes" and if not set the variable to "No". Here’s an example of what to do:
+
+# Example Statement: My dog is the cutest dog in the world.
+example_statement = "No"
+
+# Statement 1: Dogs are mammals.
+statement_one = "Yes"
+
+# Statement 2: My dog is named Pavel.
+statement_two = "Yes"
+
+# Statement 3: Dogs make the best pets.
+statement_three = "No"
+
+# Statement 4: Cats are female dogs.
+statement_four = "Yes"
