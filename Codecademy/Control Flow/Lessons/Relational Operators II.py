b03526d7fbab0e1388962829b406d419d878868e
Alfie <alfietones0673@outlook.com>
2024-02-28 14:47:39 +0000

    Create Relational Operators II.py

---
+++ Codecademy/Control Flow/Lessons/Relational Operators II.py
@@ -0,0 +1,18 @@
+# Other relational operators include; > (Greater Than), >= (Greater or Equal to), < (Less than), <= (Less or Equal to). 
+
+# 1. Create an if statement that checks if x and y are equal.
+# 2. Write an if statement that checks if the student has enough credits to graduate.
+x = 20
+y = 20
+
+# Write the first if statement here:
+if x == y:
+    print("These numbers are the same.")
+
+
+credits = 120
+
+# Write the second if statement here:
+if credits >= 120:
+    print("You have enough credits to graduate!")
+
