dca372a16aaef1b6e89c3b24de590027a5343d65
Alfie <alfietones0673@outlook.com>
2024-02-28 14:45:56 +0000

    Summary of Boolean Variables

---
+++ Codecademy/Control Flow/Lessons/Boolean Variables.py
@@ -0,0 +1,8 @@
+#Create a variable named my_baby_bool and set it equal to the string "true"
+my_baby_bool = "true"
+#Check the type of my_baby_bool
+print(type(my_baby_bool))
+#It’s not a boolean variable! Boolean values True and False always need to be capitalized and do not have quotation marks.
+my_baby_bool_two = True
+#Check the type of my_baby_bool_two.
+print(type(my_baby_bool_two))
