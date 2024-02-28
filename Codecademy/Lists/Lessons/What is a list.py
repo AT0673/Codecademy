7f3af08d588ec47bca3571e5ffa2cf68993a2402
Alfie <alfietones0673@outlook.com>
2024-02-28 16:00:50 +0000

    Create What is a list.py

---
+++ Codecademy/Lists/Lessons/What is a list.py
@@ -0,0 +1,6 @@
+# In Python, we can create a variable called heights to store these integers into a list: heights = [61, 70, 67, 64]
+# In Python, a list is one of the many built-in data structures that allows us to work with a collection of data in sequential order.
+
+heights = [61, 70, 67, 64, 65]
+
+broken_heights = [65, 71, 59, 62]
