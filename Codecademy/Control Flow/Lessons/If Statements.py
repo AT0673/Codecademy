c6553213f1d0c2889783895c4dca45f077ec6c0d
Alfie <alfietones0673@outlook.com>
2024-02-28 14:46:49 +0000

    Introduction to If Statements

---
+++ Codecademy/Control Flow/Lessons/If Statements.py
@@ -0,0 +1,13 @@
+# Enter a user name here, make sure to make it a string
+# If the user_name is Dave, it tells him to stay off my computer.
+user_name = "Dave"
+
+if user_name == "Dave":
+  print("Get off my computer Dave!")
+
+# Update the program with a second if statement so it checks for Angela’s user name as well and prints
+
+user_name = "angela_catlady_87"
+
+if user_name == "angela_catlady_87":
+  print("I know it is you, Dave! Go away!")
