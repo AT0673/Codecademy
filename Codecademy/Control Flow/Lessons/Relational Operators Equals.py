f62f20158306ebd42cb8d9c633c1fd03d15cf8ad
Alfie <alfietones0673@outlook.com>
2024-02-28 14:47:37 +0000

    Create Relational Operators Equals.py

---
+++ Codecademy/Control Flow/Lessons/Relational Operators Equals.py
@@ -0,0 +1,16 @@
+#The two relational operators we’ll cover first are:
+    #Equals: ==
+    #Not equals: !=
+#These operators compare two items and return True or False if they are equal or not.
+#We can create boolean expressions by comparing two values using these operators:
+
+# Determine if the following boolean expressions are True or False. Input your answer as True or False in the appropriate variable to the right.
+
+#Statement 1: (5 * 2) - 1 == 8 + 1
+statement_one = True
+
+#Statement 2: 13 - 6 != (3 * 2) + 1
+statement_two = False
+
+#Statement 3: 3 * (2 - 1) == 4 - 1
+statement_three = True
